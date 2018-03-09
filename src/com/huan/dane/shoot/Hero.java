eb2b485542760afcebed2d10b6303feb3d4b6abe
huan <busfool@gmail.com>
2018-03-09 18:24:03 +0800

    offwork update

---
+++ src/com/huan/dane/shoot/Hero.java
@@ -0,0 +1,11 @@
+package com.huan.dane.shoot;
+
+import java.awt.image.BufferedImage;
+
+public class Hero extends FlyingObject {
+    protected BufferedImage[] images = {};
+    protected int index = 0;
+
+    private int doubleFire;
+    private int life;
+}
---
+++ src/com/huan/dane/shoot/ShootGame.java
@@ -0,0 +1,23 @@
+package com.huan.dane.shoot;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+
+public class ShootGame extends JPanel {
+    public static final int WIDTH = 400;
+    public static final int HEIGHT = 600;
+
+    public static BufferedImage background;
+    public static BufferedImage start;
+    public static BufferedImage airplane;
+    public static BufferedImage bee;
+    public static BufferedImage bullet;
+    public static BufferedImage hero0;
+    public static BufferedImage hero1;
+    public static BufferedImage pause;
+    public static BufferedImage gameOver;
+
+    static {
+    }
+}
